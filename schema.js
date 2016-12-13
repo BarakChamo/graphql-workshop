5a660d615664f3b02884d9ca38d30613cd027ff8
Barak Chamo <barak.chamo@gmail.com>
2016-12-13 19:33:18 +0200

    workshop branch

--- schema.js
+++ schema.js
@@ -16,46 +16,7 @@ var events = require('./events.json');
     events: [Event]
  */
 
-var userType = new graphql.GraphQLObjectType({
-  name: 'User',
-  description: 'A User of the app',
-  fields: () => ({
-    id: { type: graphql.GraphQLString },
-    name: { type: graphql.GraphQLString },
-
-    /*
-      Friends resolver
-
-      Recieves a user model and returns an array of user promises
-     */
-    friends: {
-      type: new graphql.GraphQLList(userType),
-      resolve: (user, args, context) => {
-        console.log('FETCH USER');
-
-        return user.friends.map(friendId => {
-          return new Promise(resolve => resolve(users[friendId]));
-        });
-      }
-    },
-
-    /*
-      Events resolver
-
-      Recieves a user model and returns an array of event promises
-     */
-    events: {
-      type: new graphql.GraphQLList(eventType),
-      resolve: (user, args, context) => {
-        console.log('FETCH EVENT');
-
-        return user.events.map(eventId => {
-          return new Promise(resolve => resolve(events[eventId]));
-        });
-      }
-    }
-  })
-});
+var userType
 
 
 /*
@@ -69,29 +30,7 @@ var userType = new graphql.GraphQLObjectType({
     creator: User
  */
 
-var eventType = new graphql.GraphQLObjectType({
-  name: 'Event',
-  description: 'An event listing in the app',
-  fields: () => ({
-    id: { type: graphql.GraphQLString },
-    title: { type: graphql.GraphQLString },
-    time: { type: graphql.GraphQLFloat },
-
-    /*
-      Creator resolver
-
-      Recieves a user id and resolves to the user model
-     */
-    creator: {
-      type: userType,
-      resolve: (event, args, context) => {
-        console.log('FETCH USER');
-
-        return new Promise(resolve => resolve(users[event.creator]));
-      }
-    }
-  })
-});
+var eventType
 
 
 /*
@@ -107,35 +46,7 @@ var eventType = new graphql.GraphQLObjectType({
 var schema = new graphql.GraphQLSchema({
   query: new graphql.GraphQLObjectType({
     name: 'Query',
-    fields: {
-      user: {
-        type: userType,
-        args: {
-          // User id
-          id: { type: graphql.GraphQLString }
-        },
-
-        // Resolve the user from provided user id
-        resolve: (_, args) => {
-          console.log('FETCH USER');
-          return new Promise(resolve => resolve(users[args.id]));
-        }
-      },
-
-      event: {
-        type: eventType,
-        args: {
-          // Event id
-          id: { type: graphql.GraphQLString }
-        },
-
-        // Resolve the event from provided event id
-        resolve: (_, args) => {
-          console.log('FETCH EVENT');
-          return new Promise(resolve => resolve(events[args.id]));
-        }
-      }
-    }
+    fields: {}
   })
 });
 
